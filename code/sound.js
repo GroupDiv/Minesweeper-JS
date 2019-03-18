7019f4c081114f333b3b5681308dc0b34c018c77
Marcus Leong <mwleong25@gmail.com>
2019-03-18 16:16:40 -0500

    Documentation for sound.js

--- code/sound.js
+++ code/sound.js
@@ -1,6 +1,13 @@
-//sound.js
-//The following sound class was borrowed from https://www.w3schools.com/graphics/game_sound.asp
+/**
+ * sound.js
+ * Handles the sound functionality of the game
+ * The following sound class was borrowed from https://www.w3schools.com/graphics/game_sound.asp
+ */
 class sound {
+	/**
+	 * @constructor
+	 * @param {*} src 
+	 */
 	constructor(src) {
 		this.sound = document.createElement("audio");
 		this.sound.src = src;
